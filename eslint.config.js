db301645c474833fa875dd7bac39397060848c0a
hamdyyemadd <hamdyyemad@aol.com>
2025-05-03 00:44:54 +0200

    editing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,21 @@
-//  @ts-check
+// @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import eslintPluginTs from '@typescript-eslint/eslint-plugin'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': eslintPluginTs
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_'
+      }]
+    }
+  }
+]
